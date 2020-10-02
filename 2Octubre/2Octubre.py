c80d3f1432bb6e58c41e5c198108d720baa6c067
jesuspablo888 <48724931+jesuspablo888@users.noreply.github.com>
2020-10-02 16:20:11 -0500

    Add files via upload

--- 2Octubre/2Octubre.py
+++ 2Octubre/2Octubre.py
@@ -1,20 +1,57 @@
-from adt_nodos import ListaEnlazada
-
 M=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio",
      "Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-
-edo=['AGUASCALIENTES','BAJA CALIFORNIA','BAJA CALIFORNIA SUR','CAMPECHE','COAHUILA','COLIMA',
+Edo=['AGUASCALIENTES','BAJA CALIFORNIA','BAJA CALIFORNIA SUR','CAMPECHE','COAHUILA','COLIMA',
         'CHIAPAS','CHIHUAHUA','DISTRITO FEDERAL','DURANGO','GUANAJUATO','GUERRERO','HIDALGO'
         ,'JALISCO','ESTADO DE MÉXICO','MICHOACÁN','MORELOS','NAYARIT','NUEVO LEÓN','OAXACA','PUEBLA',
         'QUERÉTARO','QUINTANA ROO','SAN LUIS POTOSÍ','SINALOA','SONORA','TABASCO','TAMAULIPAS','TLAXCALA'
         ,'VERACRUZ','YUCATÁN','ZACATECAS']
-anio=str(input('Que año desea ver(2017-2018): '))
+esta_lista= []
+for anio in range(2017,2019):
+    anio_lista=[]
+    archivo=open(str(anio)+'Precip.csv','rt')
+    datos=archivo.read()
+    lineas=datos.split('\n')
+    for estado in range(2,34):
+        mes_lista=[]
+        edo=lineas[estado].split(',')
+        for mes in range(1,14):
+            mes_lista.append(edo[mes])
+        anio_lista.append(mes_lista)
+    esta_lista.append(anio_lista)
+def Suma(y,e):
+    s=0.0
+    for x in range(0,12,1):
+        s+=float(esta_lista[y-2017][e-1][x])
+    return s
+    
 estado=int(input('Que estado(1-32)?:'))
+anio=int(input("año(2017-2018)?:"))
 mes=int(input("mes(1-12)?:"))
-esta_lista= ListaEnlazada() 
-archivo=open(anio+'Precip.csv','rt',encoding='latin')
-datos=archivo.readlines()
-for l in datos:
-    l.strip()
 
+print(f"En el estado de {Edo[estado-1]} Llovió un promedio de {esta_lista[anio-2017][estado-1][mes-1]}"
+      +f" centímetros cúbicos en el mes de {M[mes-1]} de {anio}\n----------------------------------------------------")
 
+print(f"En el estado de {Edo[estado-1]} Llovió un promedio Anual de {esta_lista[anio-2017][estado-1][12]}"
+      +f" centímetros cúbicos\n----------------------------------------------------")
+print(f"La suma de los 12 meses del estado de {Edo[estado-1]} de {anio} es: {'%.2f'%Suma(anio,estado)} n----------------------------------------------------")
+print("+++++++++++++++++++++el mes que mas llovió+++++++++++++++++++++\n\n\n")
+for a in range(0,2):
+    print(f"++++++++++++++++++++++++++++{a+2017}+++++++++++++++++++++++++++")
+    for e in range(0,32):
+        ML=[]
+        for m in range(0,12,1):
+            ML.append(float(esta_lista[a][e][m]))
+        m=max(ML)
+        i=ML.index(m)
+        print(f"año: {a+2017}\nEstado: {Edo[e]}\nMes: {M[i]}\nPromedio: {m}\n-------------------------------------------\n")
+
+print("+++++++++++++++++++++el mes que menos llovió+++++++++++++++++++++\n\n\n")
+for a in range(0,2):
+    print(f"++++++++++++++++++++++++++++{a+2017}+++++++++++++++++++++++++++")
+    for e in range(0,32):
+        ML=[]
+        for m in range(0,12,1):
+            ML.append(float(esta_lista[a][e][m]))
+        m=min(ML)
+        i=ML.index(m)
+        print(f"año: {a+2017}\nEstado: {Edo[e]}\nMes: {M[i]}\nPromedio: {m}\n-------------------------------------------\n")
